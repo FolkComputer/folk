2aad870da2d37d39e99ca8dd2669ba4ad0181b1f
Omar Rizwan <omar@omar.website>
2022-02-25 06:07:37 -0500

    add matching

--- folk.tcl
+++ folk.tcl
@@ -4,14 +4,16 @@ proc Claim {args} {
     dict set ::statements $args true
 }
 proc When {args} {
-    
+    set statement [lreplace $args end end]
+
+    if [dict exists $::statements $statement] {
+        set cb [lindex $args end]
+        eval $cb
+    }
 }
 
 Claim the fox is out
 
-puts [dict size $::statements]
-puts $::statements
-
 When the fox is out {
-    
+    puts "Squeak!"
 }
