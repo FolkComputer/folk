058d2d2eb6d54f288e00f1d60f3883a1b372f913
Omar Rizwan <omar@omar.website>
2022-03-10 03:21:59 -0500

    add Wish

--- folk.tcl
+++ folk.tcl
@@ -1,7 +1,12 @@
 set ::statements [dict create]
 
 proc Claim {args} {
-    dict set ::statements $args true
+    # TODO: get the caller instead of `someone`
+    dict set ::statements [list someone claims {*}$args] true
+}
+proc Wish {args} {
+    # TODO: get the caller instead of `someone`
+    dict set ::statements [list someone wishes {*}$args] true
 }
 
 proc matches {clause statement} {
@@ -28,6 +33,9 @@ proc When {args} {
 
     dict for {statement _} $::statements {
         set match [matches $clause $statement]
+        if {$match == false} {
+            set match [matches [list /someone/ claims {*}$clause] $statement]
+        }
         if {$match != false} {
             dict with match $cb
         }
@@ -45,3 +53,19 @@ When the /animal/ is out {
 When the /animal/ is around {
     puts "hello $animal"
 }
+
+# Wish "/dev/fb0" draws a rectangle with x 0 y
+# "<window UUID>" wishes "/dev/fb0" draws a rectangle with x 0 y 0
+# (I want to invoke that from a window on my laptop which has a UUID.)
+
+When the time is /t/ {
+    Claim page 3000 has width 30 height 20
+
+    When page /pageId/ has width /width/ height /height/ {
+        # issue a draw call to vkvg
+    }
+}
+
+# with key1 /value1/ key2 /value2/
+# With all /matches/
+# To know when
