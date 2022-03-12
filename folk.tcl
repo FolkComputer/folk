4a8d77b7007df60a12f0245909f4b18d1dd243a6
Omar Rizwan <omar@omar.website>
2022-03-12 05:54:37 -0500

    start integrating graphics into folk.tcl

--- folk.tcl
+++ folk.tcl
@@ -75,17 +75,25 @@ When the /animal/ is around {
 set fb [open "/dev/fb0" w]
 fconfigure $fb -translation binary
 
-When /someone/ wishes /device/ shows a rectangle with \
-    x /x/ y /y/ width /width/ height /height/ fill /rgb/ {
-        
+set black [binary format b16 [join {00000 000000 00000} ""]]
+set blue  [binary format b16 [join {11111 000000 00000} ""]]
+set green [binary format b16 [join {00000 111111 00000} ""]]
+set red   [binary format b16 [join {00000 000000 11111} ""]]
+
+proc fbFillRect {fb x0 y0 x1 y1 color} {
+    for {set y $y0} {$y < $y1} {incr y} {
+        for {set x $x0} {$x < $x1} {incr x} {
+            puts -nonewline $fb $color
+        }
+    }
+}
+proc fbFillScreen {fb color} {
+    fbFillRect $fb 0 0 1920 1080 $color
 }
 
-func fbBlank {} {
-    # red or black
-    # write one pixel
-    []
-    binary format c4 {0 0 255 0}
-    # b g r a
+When /someone/ wishes /device/ shows a rectangle with \
+    x /x/ y /y/ width /width/ height /height/ fill /color/ {
+        fbFillRect $fb $x $y [expr $x + $width] [expr $y + $height] $color
 }
 
 # with key1 /value1/ key2 /value2/
@@ -93,9 +101,11 @@ func fbBlank {} {
 # To know when
 
 proc step {} {
+    global fb black
+
     # clear the screen
-    fbBlank
-    
+    fbFillScreen $fb $black
+
     # infinite event loop
     # event: an incoming statement bundle
     # a statement bundle includes statements and statement-retractions
