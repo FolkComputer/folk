a16d63a1f324734f093b8054ddd4700ffd888c15
Omar Rizwan <omar@omar.website>
2022-03-12 04:48:30 -0500

    framebuffer.tcl works

--- useful/framebuffer.tcl
+++ useful/framebuffer.tcl
@@ -1,7 +1,21 @@
 set fb [open "/dev/fb0" w]
 fconfigure $fb -translation binary
 
-set red [binary format c4 {0 0 255 0}]
-for {set i 0} {$i < 100} {incr i} {
-    puts $fb $red
+# $ fbset
+# mode "1920x1080"
+#     geometry 1920 1080 1920 1080 16
+#     timings 0 0 0 0 0 0 0
+#     accel true
+#     rgba 5/11,6/5,5/0,0/0
+# endmode
+
+# bgr
+# blue: set color [binary format b16 1111100000000000]
+set color [binary format b16 1111100000000000]
+for {set y 0} {$y < 1080} {incr y} {
+    for {set x 0} {$x < 1920} {incr x} {
+        puts -nonewline $fb $color
+    }
 }
+
+
