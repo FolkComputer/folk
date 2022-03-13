503f2f59231a9524c73e3fa31222cffdc784ffaf
folk0 <folk0@folk.computer>
2022-03-13 15:49:53 -0400

    make framebuffer.tcl look up screen resolution

--- useful/framebuffer.tcl
+++ useful/framebuffer.tcl
@@ -9,6 +9,16 @@ fconfigure $fb -translation binary
 #     rgba 5/11,6/5,5/0,0/0
 # endmode
 
+# $ fbset
+# mode "4096x2160"
+#     geometry 4096 2160 4096 2160 16
+#     timings 0 0 0 0 0 0 0
+#     accel true
+#     rgba 5/11,6/5,5/0,0/0
+# endmode
+
+regexp {mode "(\d+)x(\d+)"} [exec fbset] -> ::WIDTH ::HEIGHT
+
 # bgr
 set blue  [binary format b16 [join {11111 000000 00000} ""]]
 set green [binary format b16 [join {00000 111111 00000} ""]]
@@ -16,15 +26,17 @@ set green [binary format b16 [join {00000 111111 00000} ""]]
 # takes ~1,700,000 us (~1.7s)
 proc clearTcl {fb color} {
     seek $fb 0
-    for {set y 0} {$y < 1080} {incr y} {
-        for {set x 0} {$x < 1920} {incr x} {
+    for {set y 0} {$y < $::HEIGHT} {incr y} {
+        for {set x 0} {$x < $::WIDTH} {incr x} {
             puts -nonewline $fb $color
         }
     }
     seek $fb 0
 }
 
+puts {clearTcl $fb $green}
 puts [time {clearTcl $fb $green}]
+puts {clearTcl $fb $blue}
 puts [time {clearTcl $fb $blue}]
 
 # this doesn't work right, but it's close:
@@ -35,15 +47,17 @@ critcl::cproc clearC {char* fbHandle char* color} void {
     sscanf(fbHandle, "file%d", &fb);
 
     lseek(fb, 0, SEEK_SET);
-    for (int y = 0; y < 1080; y++) {
-        for (int x = 0; x < 1920; x++) {
+    for (int y = 0; y < $::HEIGHT; y++) {
+        for (int x = 0; x < $::WIDTH; x++) {
             write(fb, color, 2);
         }
     }
     lseek(fb, 0, SEEK_SET);
 }
 
+puts {clearC $fb $green}
 puts [time {clearC $fb $green}]
+puts {clearC $fb $blue}
 puts [time {clearC $fb $blue}]
 
 # ideas:
