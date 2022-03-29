aabe7333e50515206057b13c6ce7aad0cc9af4c7
Omar Rizwan <omar@omar.website>
2022-03-29 05:43:42 -0400

    sharing.tcl draft

---
+++ programs/sharing.tcl
@@ -0,0 +1,7 @@
+Wish $node shares statements matching {/someone/ claims /something/ is a rectangle with x /ax/ y /ay/ width /awidth/ height /aheight/}
+Wish $node shares statements matching {/someone/ wishes /something/ is labelled /something/}
+Wish $node shares statements matching {/someone/ wishes /something/ is highlighted /something/}
+
+With all /matches/ for /someone/ wishes $node shares statements matching /clause/ {
+    
+}
