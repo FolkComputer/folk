9cb5bdd0265bbbe28ef90b56e7d357a60f32b645
folk0 <folk0@folk.computer>
2022-05-04 21:24:03 -0400

    fix framebuffer.tcl C color

--- useful/framebuffer.tcl
+++ useful/framebuffer.tcl
@@ -20,8 +20,11 @@ fconfigure $fb -translation binary
 regexp {mode "(\d+)x(\d+)"} [exec fbset] -> ::WIDTH ::HEIGHT
 
 # bgr
+set black [binary format b16 [join {00000 000000 00000} ""]]
+set white [binary format b16 [join {11111 111111 11111} ""]]
 set blue  [binary format b16 [join {11111 000000 00000} ""]]
 set green [binary format b16 [join {00000 111111 00000} ""]]
+set red   [binary format b16 [join {00000 000000 11111} ""]]
 
 # takes ~1,700,000 us (~1.7s)
 proc clearTcl {fb color} {
@@ -34,32 +37,59 @@ proc clearTcl {fb color} {
     seek $fb 0
 }
 
-puts {clearTcl $fb $green}
-puts [time {clearTcl $fb $green}]
-puts {clearTcl $fb $blue}
-puts [time {clearTcl $fb $blue}]
+# puts {clearTcl $fb $green}
+# puts [time {clearTcl $fb $green}]
+# puts {clearTcl $fb $red}
+# puts [time {clearTcl $fb $red}]
+
 
 # this doesn't work right, but it's close:
 # (it's also not actually faster, lol)
 package require critcl
-critcl::cproc clearCInner {char* fbHandle int width int height char* color} void {
+
+critcl::cproc clearCInner {char* fbHandle int width int height bytes color} void {
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
     int fb;
-    sscanf(fbHandle, "file%d", &fb);
+  sscanf(fbHandle, "file%d", &fb);
+  printf("fbh %s fb %d\n", fbHandle, fb);
+
+  printf("color " BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN "\n", BYTE_TO_BINARY(color.s[0]), BYTE_TO_BINARY(color.s[1]));
 
     lseek(fb, 0, SEEK_SET);
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            write(fb, color, 2);
+            write(fb, color.s, 2);
         }
     }
     lseek(fb, 0, SEEK_SET);
 }
-proc clearC {fbHandle color} { clearCInner fbHandle $::WIDTH $::HEIGHT color}
+proc clearC {fbHandle color} { clearCInner $fbHandle $::WIDTH $::HEIGHT $color }
 
-puts {clearC $fb $green}
-puts [time {clearC $fb $green}]
-puts {clearC $fb $blue}
-puts [time {clearC $fb $blue}]
+# set routine {clearC $fb $green}
+# puts $routine
+# puts [time $routine]
+
+# set routine {clearC $fb $red}
+# puts $routine
+# puts [time $routine]
+
+set routine {clearC $fb $blue}
+puts $routine
+puts [time $routine]
+
+set routine {clearC $fb $red}
+puts $routine
+puts [time $routine]
 
 # ideas:
 # - clear in C
