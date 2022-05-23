2a43552fdca7e759660a97fff61cda656e2e6f71
Omar Rizwan <omar@omar.website>
2022-05-23 05:26:22 -0400

    more tcc play, camera-tcc play

--- wip/camera.tcl
+++ wip/camera.tcl
@@ -1,55 +1,109 @@
 # wip
 
-Claim "/dev/video0" is a camera
+package require tcc4tcl
 
-When /camera/ is a camera {
-    # camera_init(camera)
-    C {
-        struct v4l2_capability cap;
-        if (xioctl(camera->fd, VIDIOC_QUERYCAP, &cap) == -1) quit("VIDIOC_QUERYCAP");
-        if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) quit("no capture");
-        if (!(cap.capabilities & V4L2_CAP_STREAMING)) quit("no streaming");
+set handle [tcc4tcl::new]
+$handle process_command_line {-D__ARM_PCS_VFP=1 -I/usr/include -I/usr/include/arm-linux-gnueabihf}
 
-        struct v4l2_format format;
-        memset(&format, 0, sizeof format);
-        format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        format.fmt.pix.width = camera->width;
-        format.fmt.pix.height = camera->height;
-        format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
-        format.fmt.pix.field = V4L2_FIELD_NONE;
-        if (xioctl(camera->fd, VIDIOC_S_FMT, &format) == -1) quit("VIDIOC_S_FMT");
+$handle ccode {
+    #include <errno.h>
+    
+    #include <fcntl.h>
+    #include <sys/ioctl.h>
+    #include <sys/mman.h>
+    #include <asm/types.h>
+    #include <linux/videodev2.h>
 
-        struct v4l2_requestbuffers req;
-        memset(&req, 0, sizeof req);
-        req.count = 4;
-        req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        req.memory = V4L2_MEMORY_MMAP;
-        if (xioctl(camera->fd, VIDIOC_REQBUFS, &req) == -1) quit("VIDIOC_REQBUFS");
-        camera->buffer_count = req.count;
-        camera->buffers = calloc(req.count, sizeof (buffer_t));
+    typedef struct {
+        uint8_t* start;
+        size_t length;
+    } buffer_t;
 
-        size_t buf_max = 0;
-        for (size_t i = 0; i < camera->buffer_count; i++) {
-          struct v4l2_buffer buf;
-          memset(&buf, 0, sizeof buf);
-          buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-          buf.memory = V4L2_MEMORY_MMAP;
-          buf.index = i;
-          if (xioctl(camera->fd, VIDIOC_QUERYBUF, &buf) == -1)
-            quit("VIDIOC_QUERYBUF");
-          if (buf.length > buf_max) buf_max = buf.length;
-          camera->buffers[i].length = buf.length;
-          camera->buffers[i].start = 
-            mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, 
-               camera->fd, buf.m.offset);
-          if (camera->buffers[i].start == MAP_FAILED) quit("mmap");
-        }
-        camera->head.start = malloc(buf_max);
+    typedef struct {
+        int fd;
+        uint32_t width;
+        uint32_t height;
+        size_t buffer_count;
+        buffer_t* buffers;
+        buffer_t head;
+    } camera_t;
+
+    void quit(const char * msg)
+    {
+        fprintf(stderr, "[%s] %d: %s\n", msg, errno, strerror(errno));
+        exit(1);
     }
 
-    # camera_start(camera)
-    C {
-      for (size_t i = 0; i < camera->buffer_count; i++) {
+    int xioctl(int fd, int request, void* arg)
+    {
+        for (int i = 0; i < 100; i++) {
+            int r = ioctl(fd, request, arg);
+            printf("[%x][%d] %s\n", request, i, strerror(errno));
+            if (r != -1 || errno != EINTR) return r;
+        }
+        return -1;
+    }
+}
+
+$handle cproc cameraOpen {char* device int width int height} camera_t* {
+    int fd = open(device, O_RDWR | O_NONBLOCK, 0);
+    if (fd == -1) quit("open");
+    camera_t* camera = malloc(sizeof (camera_t));
+    camera->fd = fd;
+    camera->width = width;
+    camera->height = height;
+    camera->buffer_count = 0;
+    camera->buffers = NULL;
+    camera->head.length = 0;
+    camera->head.start = NULL;
+    return camera;
+}
+    
+$handle cproc cameraInit {camera_t* camera} void {
+    struct v4l2_capability cap;
+    if (xioctl(camera->fd, VIDIOC_QUERYCAP, &cap) == -1) quit("VIDIOC_QUERYCAP");
+    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) quit("no capture");
+    if (!(cap.capabilities & V4L2_CAP_STREAMING)) quit("no streaming");
+
+    struct v4l2_format format;
+    memset(&format, 0, sizeof format);
+    format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    format.fmt.pix.width = camera->width;
+    format.fmt.pix.height = camera->height;
+    format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+    format.fmt.pix.field = V4L2_FIELD_NONE;
+    if (xioctl(camera->fd, VIDIOC_S_FMT, &format) == -1) quit("VIDIOC_S_FMT");
+
+    struct v4l2_requestbuffers req;
+    memset(&req, 0, sizeof req);
+    req.count = 4;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+    if (xioctl(camera->fd, VIDIOC_REQBUFS, &req) == -1) quit("VIDIOC_REQBUFS");
+    camera->buffer_count = req.count;
+    camera->buffers = calloc(req.count, sizeof (buffer_t));
+
+    size_t buf_max = 0;
+    for (size_t i = 0; i < camera->buffer_count; i++) {
+        struct v4l2_buffer buf;
+        memset(&buf, 0, sizeof buf);
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        if (xioctl(camera->fd, VIDIOC_QUERYBUF, &buf) == -1)
+            quit("VIDIOC_QUERYBUF");
+        if (buf.length > buf_max) buf_max = buf.length;
+        camera->buffers[i].length = buf.length;
+        camera->buffers[i].start = 
+            mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, 
+                 camera->fd, buf.m.offset);
+        if (camera->buffers[i].start == MAP_FAILED) quit("mmap");
+    }
+    camera->head.start = malloc(buf_max);
+}
+
+$handle cproc cameraStart {camera_t* camera} void {
+    for (size_t i = 0; i < camera->buffer_count; i++) {
         struct v4l2_buffer buf;
         memset(&buf, 0, sizeof buf);
         buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
@@ -57,12 +111,18 @@ When /camera/ is a camera {
         buf.index = i;
         if (xioctl(camera->fd, VIDIOC_QBUF, &buf) == -1) quit("VIDIOC_QBUF");
         printf("camera_start(%d): %s\n", i, strerror(errno));
-      }
-
-      enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-      if (xioctl(camera->fd, VIDIOC_STREAMON, &type) == -1) 
-        quit("VIDIOC_STREAMON");
     }
-    
+
+    enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (xioctl(camera->fd, VIDIOC_STREAMON, &type) == -1) 
+        quit("VIDIOC_STREAMON");
 }
+
+$handle go
+
+set camera [cameraOpen "/dev/video0" 800 448]
+cameraInit $camera
+cameraStart $camera
+puts "camera!"
+
 
