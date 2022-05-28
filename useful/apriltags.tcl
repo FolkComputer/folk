247b6d8ab647d4eb1d0a2af6eaffd45516ace424
Omar Rizwan <omar@omar.website>
2022-05-28 16:12:43 -0400

    apriltags: Draw multiple size tags

--- useful/apriltags.tcl
+++ useful/apriltags.tcl
@@ -3,11 +3,32 @@ cd tagStandard52h13
 
 puts "[llength [glob *.png]] tags"
 
-set tagPng tag52_13_00000.png
-puts [exec identify $tagPng]
-
 set sizeInches 1
-set sizePx [expr $sizeInches * 144]
-set outPdf [exec mktemp -t test_tag].pdf
-exec convert $tagPng -filter point -resize [set sizePx]x[set sizePx] -gravity center -density 144 -extent 1224x1584\! $outPdf
-exec open $outPdf
+proc drawTag {id sizeInches} {
+    set tagPng tag52_13_[format %-05s $id].png
+    puts [exec identify $tagPng]
+
+    set sizePx [expr $sizeInches * 144]
+    set outPng [exec mktemp -t test_tag].png
+    exec convert $tagPng -filter point -resize [set sizePx]x[set sizePx] $outPng
+
+    return $outPng
+}
+
+set tagSizes [list 1 5 10]
+foreach tagSize $tagSizes {
+    lappend tagPngs [drawTag 0 $tagSize]
+    # TODO: draw text under the tag png
+}
+
+foreach tagPng $tagPngs {
+    exec open $tagPng
+}
+
+# TODO: composite with the other tag pngs?
+
+#        -gravity center -density 144 -extent 1224x1584\! $outPdf
+#    exec open $outPdf
+
+# TODO: overlay multiple fiducials of diff sizes on one page
+# TODO: draw the fiducial size under the fiducial
