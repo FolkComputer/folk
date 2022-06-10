a0d6571e394d5bae32d390e4c83257cbcd39d0c6
folk0 <folk0@folk.computer>
2022-06-10 15:41:31 -0400

    Forgot to add pi/pi.tcl !

---
+++ pi/pi.tcl
@@ -0,0 +1,26 @@
+source pi/Display.tcl
+
+Display::init
+
+package require Thread
+set ::cameraThread [thread::create]
+thread::send -async $::cameraThread [format {
+    source pi/Camera.tcl
+    Camera::init
+    AprilTags::init
+
+    while true {
+        set frame [Camera::frame]
+
+        set commands [list "Retract camera claims the camera frame is /something/" \
+                          "Assert camera claims the camera frame is \"$frame\"" \
+                          "Retract camera claims tag /something/ has center /something/ size /something/"]
+
+        set tags [AprilTags::detect [yuyv2gray $frame $Camera::WIDTH $Camera::HEIGHT]]
+        foreach tag $tags {
+            lappend commands "Assert camera claims tag [dict get $tag id] has center [dict get $tag center] size [dict get $tag size]"
+        }
+
+        thread::send -async "%s" [join $commands "\n"]
+    }
+} [thread::id]]
