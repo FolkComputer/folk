0d29eb947bfedc6fbaf4155d000b16f8cdc23370
folk0 <folk0@folk.computer>
2022-06-14 16:48:08 -0400

    pi: start working on Keyboard

---
+++ pi/Keyboard.tcl
@@ -0,0 +1,39 @@
+namespace eval Keyboard {
+    source "pi/KeyCodes.tcl"
+
+    variable kb
+
+    proc init {} {
+        variable kb
+        set kb [open "/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-mouse" r]
+        fconfigure $kb -translation binary
+    }
+
+    proc getChar {} {
+        # See https://www.kernel.org/doc/Documentation/input/input.txt
+        # https://www.kernel.org/doc/Documentation/input/event-codes.txt
+        # https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
+        #
+        # struct input_event {
+        #     struct timeval time;
+        #     unsigned short type; (should be EV_KEY = 0x01)
+        #     unsigned short code; (scancode; for example, 16 = q)
+        #     unsigned int value; (should be 1 for keypress)
+        # };
+        #
+        while 1 {
+            binary scan [read $Keyboard::kb 16] nntutunu tvSec tvUsec type code value
+            if {$type == 0x01 && $value == 1} break
+        }
+        set ch [dict get $Keyboard::KeyCodes $code]
+        puts "type $type code $code value $value ($ch)"
+        return $ch
+    }
+}
+
+if {$::argv0 eq [info script]} {
+    Keyboard::init
+    puts [Keyboard::getChar]
+    puts [Keyboard::getChar]
+    puts [Keyboard::getChar]
+}
