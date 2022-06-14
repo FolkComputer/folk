f5d39fdfca9afec00bccc556b0c520729da2ef06
Andrés Cuervo <acwervo@gmail.com>
2022-06-14 16:07:27 -0400

    Add tag number to apriltags.tcl

--- useful/apriltags.tcl
+++ useful/apriltags.tcl
@@ -4,23 +4,25 @@ cd tagStandard52h13
 puts "[llength [glob *.png]] tags"
 
 set sizeInches 1
+set tagNum 00002
 proc drawTag {id sizeInches} {
     set tagPng tag52_13_[format %-05s $id].png
     puts [exec identify $tagPng]
 
+    set tagNum 2
     set sizePx [expr $sizeInches * 172]
     set outPng [exec mktemp -t test_tag_[set sizeInches]in].png
     exec convert $tagPng -filter point -resize [set sizePx]x[set sizePx] -bordercolor white -border 20 \
-        -pointsize 24 "label:$sizeInches in" -gravity Center \
+        -pointsize 24 "label: $tagNum , $sizeInches in" -gravity Center \
         -append $outPng
 
     return $outPng
 }
 
-set tagSizes [list 1 3 5]
+set tagSizes [list 3]
 set drawnPngs [list]
 foreach tagSize $tagSizes {
-    set drawnPng [drawTag 0 $tagSize]
+    set drawnPng [drawTag $tagNum $tagSize]
     lappend drawnPngs $drawnPng
 }
 
