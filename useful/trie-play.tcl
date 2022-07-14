b5174f2f74be2b2538a4a9bd21506d5bfeb439a0
Omar Rizwan <omar@omar.website>
2022-07-14 01:20:23 -0400

    marge minimal into trie-play, start on trie matching alg

--- useful/trie-play.tcl
+++ useful/trie-play.tcl
@@ -1,4 +1,3 @@
-
 proc addStatement {s} {
     dict set ::statements $s true
     dict set ::statementsTrie {*}$s LEAF
@@ -15,95 +14,73 @@ proc Wish {args} {
 proc When {args} {
     set clause [lreplace $args end end]
     set cb [lindex $args end]
-    
-    # get local variables and serialize them
-    # (to fake lexical scope)
-    set locals [uplevel 1 {
+    set locals [uplevel 1 { # get local variables & serialize them (to fake lexical scope)
         set localNames [info locals]
         set locals [dict create]
-        foreach localName $localNames {
-            dict set locals $localName [set $localName]
-        }
+        foreach localName $localNames { dict set locals $localName [set $localName] }
         set locals
     }]
-    set when [list WHEN $clause $cb [dict merge $::currentMatchStack $locals]]
-
-    lappend ::whens $when
-}
-
-proc runWhen {clause cb enclosingMatchStack match} {
-    set ::currentMatchStack [dict merge $enclosingMatchStack $match]
-    dict with ::currentMatchStack $cb
+    lappend ::whens [list $clause $cb [dict merge $::currentMatchStack $locals]]
 }
 
 proc matches {clause statement} {
     set match [dict create]
-
     for {set i 0} {$i < [llength $clause]} {incr i} {
         set clauseWord [lindex $clause $i]
         set statementWord [lindex $statement $i]
-        if {[string index $clauseWord 0] eq "/"} {
-            set clauseVarName [string range $clauseWord 1 [expr [string length $clauseWord] - 2]]
-            set clauseVarValue $statementWord
-            dict set match $clauseVarName $clauseVarValue
-
+        if {[regexp {^/([^/]+)/$} $clauseWord -> clauseVarName]} {
+            dict set match $clauseVarName $statementWord
         } elseif {$clauseWord != $statementWord} {
             return false
         }
     }
     return $match
 }
-
+proc runWhen {clause cb enclosingMatchStack match} {
+    set ::currentMatchStack [dict merge $enclosingMatchStack $match]
+    dict with ::currentMatchStack $cb
+}
 proc evaluate {} {
-    # TODO: implement incremental evaluation
-    # there must be a function frame' that is in terms of diffs ...
-    # Claim should add a +1 diff to an append-only log ...
-    # then the evaluator can reduce over the log ...
-
     for {set i 0} {$i <= [llength $::whens]} {incr i} {
-        set when [lindex $::whens $i]
-        set clause [lindex $when 1]
-        set cb [lindex $when 2]
-        set enclosingMatchStack [lindex $when 3]
+        lassign [lindex $::whens $i] clause cb enclosingMatchStack
+        dict for {stmt _} $::statements {
+            set match [matches $clause $stmt]
+            if {$match == false} { set match [matches [list /someone/ claims {*}$clause] $stmt] }
+
+            if {$match != false} { runWhen $clause $cb $enclosingMatchStack $match }
+        }
 
         # i have a when
         # i want to walk every token in the when and use it to walk the statement trie
-
         # when /someone/ claims /page/ has program code /code/
-        # set tries [list $::statementsTrie]
-        # foreach word $clause {
-        #     set nextTries [list]
-        #     foreach trie $nextTries {
-        #         dict for {key subtrie} $trie {
-        #             if {$key == $word} {
-        #                 lappend nextTries $subtrie
-        #             }
-        #         }
-        #     }
-        #     set tries $nextTries
-        # }
-
-        dict for {statement _} $::statements {
-            set match [matches $clause $statement]
-            if {$match == false} {
-                set match [matches [list /someone/ claims {*}$clause] $statement]
+        puts "clause: $clause"
+        set tries [list $::statementsTrie]
+        foreach word $clause {
+            puts "tries: $tries"
+            set nextTries [list]
+            foreach trie $tries {
+                puts "trie: $trie"
+                dict for {key subtrie} $trie {
+                    puts "  compare key $key = word $word"
+                    if {[regexp {^/([^/]+)/$} $word -> clauseVarName]} {
+                        lappend nextTries $subtrie
+                    } elseif {$key == $word} {
+                        lappend nextTries $subtrie
+                    }
+                }
             }
-            if {$match != false} {
-                runWhen $clause $cb $enclosingMatchStack $match
-            }
+            set tries $nextTries
         }
+        puts "FINAL: $tries"
     }
 }
-
 proc Step {cb} {
     # clear the statement set
     set ::statements [dict create]
     set ::statementsTrie [dict create]
     set ::whens [list]
-
     set ::currentMatchStack [dict create]
-
-    uplevel 1 $cb
+    uplevel 1 $cb ;# run the body code
 
     while 1 {
         set prevStatements $::statements
@@ -114,8 +91,12 @@ proc Step {cb} {
 
 Step {
     Claim George is a dog
-    When /name/ is a /animal/ {
+    When /someone/ claims /name/ is a /animal/ {
         puts "found an animal $name"
     }
     Claim Bob is a cat
 }
+
+# $ tclsh useful/minimal-system.tcl
+# found an animal George
+# found an animal Bob
