1984f1f49d5fb5ec77aea1f76aa71522d573f052
Omar Rizwan <omar@omar.website>
2022-07-20 16:38:01 -0400

    add id and date to stamp

--- useful/apriltag-program.tcl
+++ useful/apriltag-program.tcl
@@ -68,12 +68,21 @@ proc programToPs {id text} {
             set ret
         }] "\n"]
 
+        gsave
         [expr $PageWidth-$tagwidth-$margin] [expr $PageHeight-$tagheight-$margin] translate
         $tagwidth $tagheight scale
         10 10 8 \[10 0 0 -10 0 10\]
         {<
 $image
         >} image
+        grestore
+
+        /Helvetica-Narrow findfont
+        10 scalefont
+        setfont
+        newpath
+        [expr $PageWidth-$tagwidth-$margin] [expr $PageHeight-$tagheight-16-$margin] moveto
+        ($id ([clock format [clock seconds] -format "%a, %d %b %Y, %r"])) show
     }]
 }
 
@@ -85,35 +94,3 @@ When tag /tag/ has center /c/ size /size/ {
     Wish $tag is highlighted green
 }
 }]]
-
-# cd ~/aux/apriltag-imgs ;# https://github.com/AprilRobotics/apriltag-imgs
-# cd tagStandard52h13
-
-# puts "[llength [glob *.png]] tags"
-
-# set sizeInches 1
-# proc drawTag {id sizeInches} {
-#     set tagPng tag52_13_[format %05s $id].png
-#     puts [exec identify $tagPng]
-
-#     set sizePx [expr $sizeInches * 172]
-#     set outPng [exec mktemp -t test_tag_[set sizeInches]in].png
-#     exec convert $tagPng -filter point -resize [set sizePx]x[set sizePx] -bordercolor white -border 20 \
-#         -pointsize 24 "label: $id ($sizeInches in)" -gravity Center \
-#         -append $outPng
-
-#     return $outPng
-# }
-
-# set tagId 3
-
-# set tagSizes [list 3]
-# set drawnPngs [list]
-# foreach tagSize $tagSizes {
-#     set drawnPng [drawTag $tagId $tagSize]
-#     lappend drawnPngs $drawnPng
-# }
-
-# set outPdf [exec mktemp -t test_tags].pdf
-# exec magick {*}$drawnPngs -gravity center -density 144 -extent 1224x1584\! $outPdf
-# exec open $outPdf
