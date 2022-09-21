3d04a70f2354d56e22b8dbc4f18cdb04a218c143
Omar Rizwan <omar@omar.website>
2022-09-21 00:01:37 -0400

    more work on bidirectional (when/statement) matching

--- useful/bidirectional-matching-play.tcl
+++ useful/bidirectional-matching-play.tcl
@@ -1,20 +1,37 @@
 set ::statements [dict create \
                       [list the time is 3] true \
                       [list when the time is /t/ { puts t }] true ]
-puts $::statements
+puts "statements: $::statements"
 
-proc matchAgainstExistingStatements {clause} {
-    dict for {stmt _} $::statements {
-        set match [matches $clause $stmt]
-        
+proc matchPattern {pattern statement} {
+    set match [dict create]
+    for {set i 0} {$i < [llength $pattern]} {incr i} {
+        set patternWord [lindex $pattern $i]
+        set statementWord [lindex $statement $i]
+        if {[regexp {^/([^/]+)/$} $patternWord -> patternVarName]} {
+            dict set match $patternVarName $statementWord
+        } elseif {$patternWord != $statementWord} {
+            return false
+        }
     }
+    return $match
+}
+proc matchAgainstExistingStatements {pattern} {
+    set matches [list]
+    dict for {stmt _} $::statements {
+        set match [matchPattern $pattern $stmt]
+        if {$match != false} {
+            lappend matches $match
+        }
+    }
+    return $matches
 }
 
-proc whenize {clause} {
-
+proc whenize {pattern} {
+    return [list when {*}$pattern /body/]
 }
-proc unwhenize {clause} {
-
+proc unwhenize {pattern} {
+    return [lreplace [lreplace $pattern end end] 0 0]
 }
 
 puts [matchAgainstExistingStatements [list the time is 3]]
