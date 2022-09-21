ce67e2175cfb27a107b203c18c9b54e9a6fe53ee
Omar Rizwan <omar@omar.website>
2022-09-21 01:12:33 -0400

    bidirectional matching

--- useful/bidirectional-matching-play.tcl
+++ useful/bidirectional-matching-play.tcl
@@ -1,18 +1,20 @@
 set ::statements [dict create \
                       [list the time is 3] true \
-                      [list when the time is /t/ { puts t }] true ]
+                      [list when the time is /t/ { puts $t }] true ]
 puts "statements: $::statements"
 
-proc matchPattern {pattern statement} {
-    if {[llength $pattern] != [llength $statement]} { return false }
+proc unify {a b} {
+    if {[llength $a] != [llength $b]} { return false }
 
     set match [dict create]
-    for {set i 0} {$i < [llength $pattern]} {incr i} {
-        set patternWord [lindex $pattern $i]
-        set statementWord [lindex $statement $i]
-        if {[regexp {^/([^/]+)/$} $patternWord -> patternVarName]} {
-            dict set match $patternVarName $statementWord
-        } elseif {$patternWord != $statementWord} {
+    for {set i 0} {$i < [llength $a]} {incr i} {
+        set aWord [lindex $a $i]
+        set bWord [lindex $b $i]
+        if {[regexp {^/([^/]+)/$} $aWord -> aVarName]} {
+            dict set match $aVarName $bWord
+        } elseif {[regexp {^/([^/]+)/$} $bWord -> bVarName]} {
+            dict set match $bVarName $aWord
+        } elseif {$aWord != $bWord} {
             return false
         }
     }
@@ -21,7 +23,7 @@ proc matchPattern {pattern statement} {
 proc matchAgainstExistingStatements {pattern} {
     set matches [list]
     dict for {stmt _} $::statements {
-        set match [matchPattern $pattern $stmt]
+        set match [unify $pattern $stmt]
         if {$match != false} {
             lappend matches $match
         }
@@ -30,7 +32,7 @@ proc matchAgainstExistingStatements {pattern} {
 }
 
 proc whenize {pattern} {
-    return [list when {*}$pattern /body/]
+    return [list when {*}$pattern /__body/]
 }
 proc unwhenize {pattern} {
     return [lreplace [lreplace $pattern end end] 0 0]
@@ -45,6 +47,11 @@ proc assertEq {a b} {
 assertEq [matchAgainstExistingStatements [list the time is 3]] {{}}
 assertEq [matchAgainstExistingStatements [list the time is]] {}
 assertEq [matchAgainstExistingStatements [list the time is /t/]] {{t 3}}
+
+assertEq [unwhenize [whenize {the time is /t/}]] {the time is /t/}
+
+assertEq [matchAgainstExistingStatements [unwhenize [list when the time is /t/ {puts $t}]]] {{t 3}}
+assertEq [matchAgainstExistingStatements [whenize {the time is 10}]] {{t 10 __body { puts $t }}}
 
 # question: is set-set match more efficient or 1-at-a-time match?
 # probably 1-at-a-time match is fine
