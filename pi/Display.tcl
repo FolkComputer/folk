6f5d006c26023c3e0306913abcf5451c812b52c3
Omar Rizwan <omar@omar.website>
2022-10-30 15:39:03 -0400

    Fix NUC 32-bit colors

--- pi/Display.tcl
+++ pi/Display.tcl
@@ -140,11 +140,19 @@ namespace eval Display {
     variable HEIGHT
     variable DEPTH
 
-    variable black [binary format b16 [join {00000 000000 00000} ""]]
-    variable blue  [binary format b16 [join {11111 000000 00000} ""]]
-    variable green [binary format b16 [join {00000 111111 00000} ""]]
-    variable red   [binary format b16 [join {00000 000000 11111} ""]]
-    variable white [binary format b16 [join {11111 111111 11111} ""]]
+    if {$Display::DEPTH == 16} {
+        variable black [binary format b16 [join {00000 000000 00000} ""]]
+        variable blue  [binary format b16 [join {11111 000000 00000} ""]]
+        variable green [binary format b16 [join {00000 111111 00000} ""]]
+        variable red   [binary format b16 [join {00000 000000 11111} ""]]
+        variable white [binary format b16 [join {11111 111111 11111} ""]]
+    } elseif {$Display::DEPTH == 32} {
+        variable black [binary format b16 [join {0000000 00000000 00000000} ""]]
+        variable blue  [binary format b16 [join {1111111 00000000 00000000} ""]]
+        variable green [binary format b16 [join {0000000 11111111 00000000} ""]]
+        variable red   [binary format b16 [join {0000000 00000000 11111111} ""]]
+        variable white [binary format b16 [join {1111111 11111111 11111111} ""]]
+    }
 
     variable fb
 
