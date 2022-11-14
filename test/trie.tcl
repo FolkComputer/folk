7bbd6b80e240f44c46898265e1f0f89532fef749
Omar Rizwan <omar@omar.website>
2022-11-14 13:50:58 -0500

    ctrie: Test changes

--- test/trie.tcl
+++ test/trie.tcl
@@ -9,8 +9,10 @@ proc assert condition {
    }
 }
 
+exec dot -Tpdf <<[ctrie dot $t] >ctrie.pdf
 assert {[trie lookup $t {Omar is a person}] == {1}}
+puts [trie lookup $t {/someone/ is a person}]
 assert {[trie lookup $t {/someone/ is a person}] == {1 2}}
 assert {[trie lookup $t {Omar is a /species/}] == {1}}
 assert {[trie lookup $t {Generic is a dog}] == {2}}
-exec dot -Tpdf <<[ctrie dot [ctrie tclify $t]] >ctrie.pdf
+exec dot -Tpdf <<[ctrie dot $t] >ctrie.pdf
