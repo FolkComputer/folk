8b502333f6f6083272b0650804f5131d3fdf2881
Omar Rizwan <omar@omar.website>
2022-12-15 00:36:08 -0500

    Multilabel????

--- test/with-all.tcl
+++ test/with-all.tcl
@@ -7,27 +7,32 @@ Assert program1 has program code {
 	Step
     }
     When /someone/ wishes to collect matches for /clause/ {
-         When {*}$clause {
-             puts "$clause -> $__env"
+        set varNames [lmap word $clause {expr {
+            [regexp {^/([^/ ]+)/$} $word -> varName] ? $varName : [continue]
+        }}]
+        When {*}$clause {
+            set match [dict create]
+            foreach varName $varNames { dict set match $varName [set $varName] }
 
-		dict set ::collectedMatches $clause $__env true
-		::updateCollectedMatches $clause
+            dict set ::collectedMatches $clause $match true
+            ::updateCollectedMatches $clause
 
-		On unmatch [subst {
-			dict unset ::collectedMatches {$clause} {$__env}
-			::updateCollectedMatches {$clause}
-		}]
+            On unmatch [subst {
+                dict unset ::collectedMatches {$clause} {$__env}
+                ::updateCollectedMatches {$clause}
+            }]
 	}
+        On unmatch { dict unset ::collectedMatches $clause }
     }
 }
 
-Assert program3 has program code {
+Assert program2 has program code {
     Claim Omar lives in "Oakland"
 }
-Assert program2 has program code {
+Assert program3 has program code {
     Wish to collect matches for [list Omar lives in /place/]
 }
-Assert program3 has program code {
+Assert program4 has program code {
     Claim Omar lives in "New York"
 }
 
