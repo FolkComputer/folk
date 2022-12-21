cbc8b62745e56ba9aa03fd8c7a3077c16d861c95
Cristóbal Sciutto <cristobal.sciutto@gmail.com>
2022-12-21 11:08:33 -0500

    set clang flag based on mac version

--- lib/c.tcl
+++ lib/c.tcl
@@ -129,7 +129,10 @@ namespace eval c {
             variable cflags [switch $tcl_platform(os) {
                 Darwin { expr { [file exists "$::tcl_library/../../Tcl"] ?
                                 [list -I$::tcl_library/../../Headers $::tcl_library/../../Tcl] :
-                                [list -I$::tcl_library/../../include $::tcl_library/../libtcl8.6.dylib]
+                                [list -I$::tcl_library/../../include \
+                                       $::tcl_library/../libtcl8.6.dylib \
+                                       -mmacosx-version-min=$tcl_platform(osVersion) \
+                                ]
                             } }
                 Linux { list -I/usr/include/tcl8.6 -ltcl8.6 }
             }]
