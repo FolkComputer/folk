af8c2274ce0019089208e37e8b8f1640cc1ed5ab
Omar Rizwan <omar@omar.website>
2022-12-23 15:42:56 -0500

    Improve basic test

--- test/basic.tcl
+++ test/basic.tcl
@@ -11,8 +11,10 @@ proc count condition {
 Assert programOakland has program code {
     Claim Omar lives in "Oakland"
 }
-Assert when Omar lives in /place/ {
-    Claim $place is a place where Omar lives
+Assert when $::nodename has step count /c/ {
+    When Omar lives in /place/ {
+        Claim $place is a place where Omar lives
+    }
 }
 Assert programNewYork has program code {
     Claim Omar lives in "New York"
@@ -27,3 +29,24 @@ Assert programNewJersey has program code {
 Step
 
 assert {[count [list /someone/ claims /p/ is a place where Omar lives]] == 3}
+
+set ::outlinecolors [dict create]
+Assert someone wishes BlueThing is outlined blue
+Assert someone claims BlueThing has region BlueThingRegion
+Assert when /someone/ wishes /thing/ is outlined /color/ {
+    When $thing has region /r/ {
+        dict set ::outlinecolors $thing $color
+    }
+}
+Assert someone wishes GreenThing is outlined green
+Assert someone claims GreenThing has region GreenThingRegion
+Step
+
+assert {$::outlinecolors eq "BlueThing blue GreenThing green"}
+
+Assert someone claims BlueThing has region BlueThingRegion2
+Retract someone claims BlueThing has region BlueThingRegion
+Step
+
+assert {$::outlinecolors eq "BlueThing blue GreenThing green"}
+
