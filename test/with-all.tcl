86d63646172ee35e9d9ac646c5829a457751d9d3
Omar Rizwan <omar@omar.website>
2023-02-13 18:28:26 -0500

    with-all: Add test for the unremoved label bug

--- test/with-all.tcl
+++ test/with-all.tcl
@@ -36,3 +36,26 @@ Step
 
 assert {[llength [Statements::findMatches [list /someone/ claims there are /n/ matches]]] == 1}
 assert {[llength [Statements::findMatches [list /someone/ claims there are 1 matches]]] == 1}
+
+Retract programNewYork has program code /something/
+Step
+assert {[llength [Statements::findMatches [list /someone/ claims there are /n/ matches]]] == 1}
+assert {[llength [Statements::findMatches [list /someone/ claims there are 1 matches]]] == 0}
+assert {[llength [Statements::findMatches [list /someone/ claims there are 0 matches]]] == 1}
+
+Assert labeller has program code {
+    When labelling is on /k/ {
+        When the collected matches for [list /someone/ wishes /p/ is labelled /label/] are /matches/ {
+            Claim the total label on $k is [lmap m $matches {dict get $m label}]
+        }
+    }
+}
+Assert programWithLabels has program code {
+    Wish $this is labelled "Label One"
+    Wish $this is labelled "Label Two"
+}
+Assert labelling is on A
+Retract labelling is on A
+Assert labelling is on B
+Step
+assert {[llength [Statements::findMatches [list /someone/ claims the total label on /k/ is /l/]]] == 1}
