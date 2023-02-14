344125d507523ee933d875f6ae910b79cf49f995
Omar Rizwan <omar@omar.website>
2023-02-14 01:37:56 -0500

    process: Show stdout/stderr as label.

--- lib/process.tcl
+++ lib/process.tcl
@@ -10,21 +10,30 @@ proc On-process {name body} {
 
     # TODO: send it the serialized environment
     # TODO: establish I/O w/o stdout/stdin
-    set stdio [open "|tclsh8.6 $tclfile 2>@stderr" w+]
+    set stdio [open "|tclsh8.6 $tclfile 2>@1" w+]
     set pid [pid $stdio]
 
-    set ::onScript$pid ""
-    proc ::rl {pid stdio} {
+    set ::processlog$pid [list]
+    proc ::rl {pid name stdio} {
         if {[gets $stdio line] >= 0} {
-            append ::onScript$pid $line
-            if {[info complete [set ::onScript$pid]]} {
-                puts $stdio [eval [set ::onScript$pid]]
-                set ::onScript$pid ""
-            }
+            lappend ::processlog$pid $line
+            Retract process $name has standard output log /l/
+            Assert process $name has standard output log [set ::processlog$pid]
+            Step
         } elseif {[eof $stdio]} { close $stdio }
     }
     fconfigure $stdio -blocking 0 -buffering line
-    fileevent $stdio readable [list ::rl $pid $stdio]
+    fileevent $stdio readable [list ::rl $pid $name $stdio]
 
-    On unmatch [list exec kill $pid]
+    upvar this this
+    if {[info exists this]} {
+        Assert $this is running process $name
+    }
+
+    proc ::processunmatch {pid name} {
+        exec kill $pid
+        Retract /someone/ is running process $name
+        Retract process $name has standard output log /something/
+    }
+    On unmatch [list ::processunmatch $pid $name]
 }
--- test/process.tcl
+++ test/process.tcl
@@ -1,7 +1,16 @@
 Assert when we are running {
+    set this T00
     On process A {
         puts hello
     }
 }
 Assert we are running
 Step
+
+after 500 {
+    dict for {id stmt} $Statements::statements { puts [statement short $stmt] }
+
+    exit 0
+}
+
+vwait forever
