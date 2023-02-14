ac3da254ae216c2c21fdd0925970f9d30e5766eb
Omar Rizwan <omar@omar.website>
2023-02-14 01:57:39 -0500

    process: Refactor to use per-process namespace

--- lib/process.tcl
+++ lib/process.tcl
@@ -1,39 +1,50 @@
 proc On-process {name body} {
-    set tclfd [file tempfile tclfile tclfile.tcl]
-    puts $tclfd [join [list {
-        source "main.tcl"
-        proc every {ms body} {
-            try $body
-            after $ms [list after idle [namespace code [info level 0]]]
+    namespace eval ::Processes::$name {}
+    set ::Processes::${name}::name $name
+    set ::Processes::${name}::body $body
+    set ::Processes::${name}::this [uplevel {set this}]
+    namespace eval ::Processes::$name {
+        variable tclfd [file tempfile tclfile tclfile.tcl]
+        puts $tclfd [join [list {
+            source "main.tcl"
+            proc every {ms body} {
+                try $body
+                after $ms [list after idle [namespace code [info level 0]]]
+            }
+        } $body] "\n"]; close $tclfd
+
+        # TODO: send it the serialized environment
+        # TODO: establish I/O w/o stdout/stdin
+        variable stdio [open "|tclsh8.6 $tclfile 2>@1" w+]
+        variable pid [pid $stdio]
+
+        variable log [list]
+        proc handleReadable {} {
+            variable name
+            variable stdio
+            variable log
+            if {[gets $stdio line] >= 0} {
+                lappend log $line
+                Retract process $name has standard output log /l/
+                Assert process $name has standard output log $log
+                Step
+            } elseif {[eof $stdio]} { close $stdio }
         }
-    } $body] "\n"]; close $tclfd
+        fconfigure $stdio -blocking 0 -buffering line
+        fileevent $stdio readable [namespace code handleReadable]
 
-    # TODO: send it the serialized environment
-    # TODO: establish I/O w/o stdout/stdin
-    set stdio [open "|tclsh8.6 $tclfile 2>@1" w+]
-    set pid [pid $stdio]
+        if {[info exists this]} {
+            Assert $this is running process $name
+        }
 
-    set ::processlog$pid [list]
-    proc ::rl {pid name stdio} {
-        if {[gets $stdio line] >= 0} {
-            lappend ::processlog$pid $line
-            Retract process $name has standard output log /l/
-            Assert process $name has standard output log [set ::processlog$pid]
-            Step
-        } elseif {[eof $stdio]} { close $stdio }
+        proc handleUnmatch {} {
+            variable pid
+            variable name
+            catch {exec kill $pid}
+            Retract /someone/ is running process $name
+            Retract process $name has standard output log /something/
+            namespace delete ::Processes::$name
+        }
+        On unmatch [namespace code handleUnmatch]
     }
-    fconfigure $stdio -blocking 0 -buffering line
-    fileevent $stdio readable [list ::rl $pid $name $stdio]
-
-    upvar this this
-    if {[info exists this]} {
-        Assert $this is running process $name
-    }
-
-    proc ::processunmatch {pid name} {
-        exec kill $pid
-        Retract /someone/ is running process $name
-        Retract process $name has standard output log /something/
-    }
-    On unmatch [list ::processunmatch $pid $name]
 }
