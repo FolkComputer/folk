38a177e6a54dcfbed30ca0b35ece889e77d45daa
Naveen Michaud-Agrawal <naveen.michaudagrawal@gmail.com>
2023-05-24 22:29:51 -0400

    Don't stroke if both points are equal

--- pi/Display.tcl
+++ pi/Display.tcl
@@ -246,7 +246,7 @@ namespace eval Display {
         for {set i 0} {$i < [llength $points]} {incr i} {
             set a [lindex $points $i]
             set b [lindex $points [expr $i+1]]
-            if {$b == ""} break
+            if {$b == "" || $a eq $b} break
 
             # if line is past edge of screen, clip it to the nearest
             # point along edge of screen
