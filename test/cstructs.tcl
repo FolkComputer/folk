db5e849f52f7256d4ab11fef955851f8d7914fdb
Omar Rizwan <omar@omar.website>
2023-06-18 18:14:17 -0400

    Add runtime arg typechecks to the C FFI

--- test/cstructs.tcl
+++ test/cstructs.tcl
@@ -25,3 +25,19 @@ $cc compile
 
 puts [omar]
 assert {[dict get [omar] name last] eq "Rizwan"}
+
+set cc [c create]
+$cc proc plusone {int a} int {
+    return a + 1;
+}
+$cc proc dostuff {void* v} int {
+    return 300;
+}
+$cc compile
+assert {[plusone 3] eq 4}
+
+catch {plusone Wrong} err
+assert {[string match {expected integer but got "Wrong"*} $err]}
+
+catch {dostuff hi} err
+assert {[string match {failed to convert argument from Tcl to C*} $err]}
