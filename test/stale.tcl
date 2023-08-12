3047b5b0675db8c1119750341b81e7519e5f8808
Omar Rizwan <omar@omar.website>
2023-08-12 17:31:32 -0400

    test/stale: Simplify

--- test/stale.tcl
+++ test/stale.tcl
@@ -1,12 +1,4 @@
-Assert when we are running {{} {
-    When the iteration count is /k/ {
-        Commit { Claim the counted iteration count is $k }
-    }
-}}
-Assert we are running
-Step
-
-for {set i 0} {$i < 10000} {incr i} {
+for {set i 0} {$i < 30000} {incr i} {
     Commit [list Claim the iteration count is $i]
     Step
 }
