42b780fd3aeb8ad6541de05eacb4e7202ccfca5e
Zach Potter <zspotting@gmail.com>
2023-08-20 23:26:55 -0700

    Rework keyboard and implement global terminal

--- pi/Keyboard.tcl
+++ pi/Keyboard.tcl
@@ -33,7 +33,8 @@ namespace eval Keyboard {
         fconfigure $kb -translation binary
     }
 
-    proc getChar {} {
+    # Returns a tuple of [keycode up/down/repeated]
+    proc getKeyEvent {} {
         # See https://www.kernel.org/doc/Documentation/input/input.txt
         # https://www.kernel.org/doc/Documentation/input/event-codes.txt
         # https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
@@ -42,27 +43,23 @@ namespace eval Keyboard {
         #     struct timeval time;
         #     unsigned short type; (should be EV_KEY = 0x01)
         #     unsigned short code; (scancode; for example, 16 = q)
-        #     unsigned int value; (should be 1 for keypress)
+        #     unsigned int value; (0 for key release, 1 for press, 2 for repeat)
         # };
         #
         while 1 {
-            binary scan [read $Keyboard::kb 16] nntutunu tvSec tvUsec type code value
-            if {$type == 0x01 && $value == 1} break
+            # TODO: is this CPU dependant? Originally was 16 bytes with 32-bit longs
+            # Could check [getconf LONG_BIT] for 32 or 64...
+            binary scan [read $Keyboard::kb 24] wwsss tvSec tvUsec type code value
+            if {$type == 0x01} {
+                return [list $code $value]
+            }
         }
-        # TODO: Should properly catch this error
-        # e.g. Jan 28 04:11:28 folk0 make[1991]: Thread error: tid0x7f45fd2b0640 can't\ read\ \"name\":\ no\ such\ variable ...
-
-        # scancode name, like KEY_A
-        catch { set name [dict get $Keyboard::KeyCodes $code] } err
-        set ch [string tolower [string range $name 4 end]]
-        # puts "type $type code $code value $value ($ch)"
-        return $ch
     }
 }
 
-if {[info exists ::argv0] && $::argv0 eq [info script]} {
-    Keyboard::init
-    puts [Keyboard::getChar]
-    puts [Keyboard::getChar]
-    puts [Keyboard::getChar]
-}
+# if {[info exists ::argv0] && $::argv0 eq [info script]} {
+#     Keyboard::init
+#     puts [Keyboard::getChar]
+#     puts [Keyboard::getChar]
+#     puts [Keyboard::getChar]
+# }
--- pi/pi.tcl
+++ pi/pi.tcl
@@ -5,18 +5,44 @@ thread::errorproc errorproc
 try {
     set keyboardThread [thread::create [format {
         source "pi/Keyboard.tcl"
+        source "pi/KeyCodes.tcl"
         source "lib/c.tcl"
         source "pi/cUtils.tcl"
         Keyboard::init
         puts "Keyboard tid: [getTid]"
 
-        set chs [list]
+        set keyStates [list up down repeat]
+        set modifiers [dict create \
+            shift 0 \
+            ctrl 0 \
+            alt 0 \
+        ]
+
         while true {
-            lappend chs [Keyboard::getChar]
+            lassign [Keyboard::getKeyEvent] keyCode eventType
+
+            set shift [dict get $modifiers shift]
+            set key [keyFromCode $keyCode $shift]
+            set keyState [lindex $keyStates $eventType]
+
+            set isDown [expr {$keyState != "up"}]
+            if {[string match *SHIFT $key]} {
+              dict set modifiers shift $isDown
+            }
+            if {[string match *CTRL $key]} {
+              dict set modifiers ctrl $isDown
+            }
+            if {[string match *ALT $key]} {
+              dict set modifiers alt $isDown
+            }
+
+            set heldModifiers [dict keys [dict filter $modifiers value 1]]
+            if {[llength $heldModifiers] == 0} {set heldModifiers " "}
 
             thread::send -async "%s" [subst {
-                Retract keyboard claims the keyboard character log is /something/
-                Assert keyboard claims the keyboard character log is "$chs"
+                # TODO: can't use open-square-bracket or backslash keys
+                Retract keyboard claims key /k/ is /t/ with modifiers /m/
+                Assert keyboard claims key "$key" is $keyState with modifiers "$heldModifiers"
             }]
         }
     } [thread::id]]]
