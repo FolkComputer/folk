4c5320406cb87181560168ed2b402bffe265f5b9
Omar Rizwan <omar@omar.website>
2023-09-12 19:50:54 -0400

    Display_vk: Animate images

--- play/Display_vk.tcl
+++ play/Display_vk.tcl
@@ -1258,21 +1258,27 @@ if {[info exists ::argv0] && $::argv0 eq [info script] || \
     # FIXME: bounding box for scissors
     # FIXME: sampler2D, text
 
-    # set im [Gpu::ImageManager::copyImageToGpu [image rechannel [image loadJpeg "/Users/osnr/Downloads/u9.jpg"] 4]]
+    set im [Gpu::ImageManager::copyImageToGpu [image rechannel [image loadJpeg "/Users/osnr/Downloads/u9.jpg"] 4]]
     set im2 [Gpu::ImageManager::copyImageToGpu [image rechannel [image loadJpeg "/Users/osnr/Downloads/793.jpg"] 4]]
 
-    Gpu::drawStart
-    # Gpu::draw $circle {200 50} 30
-    # Gpu::draw $circle {300 300} 20
-    # Gpu::draw $line {0 0} {100 100} 10
-    # Gpu::draw $redOnRight
-    Gpu::draw $image $im2 {0 0} {200 0} {200 200} {0 200}
-    Gpu::draw $line {0 0} {200 0} 10
-    Gpu::draw $line {200 0} {200 200} 10
-    Gpu::draw $line {200 200} {0 200} 10
-    Gpu::draw $line {0 200} {0 0} 10
+    set t 0
+    while 1 {
+        Gpu::drawStart
 
-    Gpu::drawEnd
+        # Gpu::draw $circle {200 50} 30
+        # Gpu::draw $circle {300 300} 20
+        # Gpu::draw $line {0 0} {100 100} 10
+        # Gpu::draw $redOnRight
+        Gpu::draw $image $im2 {0 0} {200 0} {200 200} {0 200}
+        Gpu::draw $image $im [list [expr {sin($t/300.0)*300}] 300] {400 300} {400 400} {200 400}
+        Gpu::draw $line {0 0} {200 0} 10
+        Gpu::draw $line {200 0} {200 200} 10
+        Gpu::draw $line {200 200} {0 200} 10
+        Gpu::draw $line {0 200} {0 0} 10
 
-    while 1 { Gpu::poll }
+        Gpu::drawEnd
+
+        Gpu::poll
+        incr t
+    }
 }
