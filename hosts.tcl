3533d4b9b70da87472e30f485e0614d92cffbf73
Andrés Cuervo <andrescuervor@gmail.com>
2023-09-22 12:44:26 -0400

    Update folk-cwervo host wifi

--- hosts.tcl
+++ hosts.tcl
@@ -20,7 +20,7 @@ if {[info exists ::env(FOLK_SHARE_NODE)]} {
 
         if {$wifi eq "cynosure"} {
             set ::shareNode "folk-omar.local"
-        } elseif {$wifi eq "Verizon_TWRHB4"} {
+        } elseif {$wifi eq "Verizon_TWRHB4" || $wifi eq "nyu"} {
             set ::shareNode "folk-cwervo.local"
         } elseif {$wifi eq "WONDERLAND"} {
             set ::shareNode "folk-haip.local"
