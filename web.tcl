e0684dd91815e7c5b4ddf95b5e2182515ddc4b98
Omar Rizwan <omar@omar.website>
2023-09-30 15:23:51 -0400

    web: Fix html, body, editor

--- web.tcl
+++ web.tcl
@@ -156,8 +156,8 @@ proc handleRead {chan addr port} {
             set route [dict get $match route]
             set handler [dict get $match handler]
             if {[regexp -all $route $path whole_match]} {
-                set ^html {{body} {dict create statusAndHeaders "HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/html; charset=utf-8\n\n" body $body}}
-                set ^json {{body} {dict create statusAndHeaders "HTTP/1.1 200 OK\nConnection: close\nContent-Type: application/json; charset=utf-8\n\n" body $body}}
+                fn html {body} {dict create statusAndHeaders "HTTP/1.1 200 OK\nConnection: close\nContent-Type: text/html; charset=utf-8\n\n" body $body}
+                fn json {body} {dict create statusAndHeaders "HTTP/1.1 200 OK\nConnection: close\nContent-Type: application/json; charset=utf-8\n\n" body $body}
                 set response [apply [list {path ^html ^json} $handler] $path ${^html} ${^json}]
             }
         }
