977ea648e836232c2af2119c7bdc0bf1c4aa5128
Omar Rizwan <omar@omar.website>
2023-12-05 11:56:41 -0500

    refine,calibrate-play: Support running offline on Mac

--- play/calibrate-play.tcl
+++ play/calibrate-play.tcl
@@ -2,14 +2,24 @@ lappend auto_path "./vendor"
 source "lib/language.tcl"
 namespace eval Evaluator { source "lib/environment.tcl" }
 proc Wish {args} {}
-proc When {args} {}
+proc When {args} {
+    if {[lrange $args 0 end-1] eq [list the calibration model is /MODEL/]} {
+        set body [lindex $args end]
+        set MODEL $::MODEL
+        eval $body
+        set ::refineCalibration $refineCalibration
+    }
+}
+proc Claim {args} {}
 
 apply {{} {
-    source "virtual-programs/calibrate/calibrate.folk"
     source "lib/c.tcl"
+    source "virtual-programs/calibrate/calibrate.folk"
+    set ::MODEL $MODEL
+    source "virtual-programs/calibrate/refine.folk"
     source "virtual-programs/calibrate/calibration-test.folk"
 
     set fd [open "folk-calibration-poses.txt" r]
     set calibrationPoses [read $fd]; close $fd
-    calibrate $calibrationPoses
+    calibrate $calibrationPoses $::refineCalibration
 }}
