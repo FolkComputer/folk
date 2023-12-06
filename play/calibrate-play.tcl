de551aeba8e96267e0cbda4f484b764c0833f78d
Omar Rizwan <omar@omar.website>
2023-12-06 15:18:35 -0500

    calibrate-play: WIP: Emit camera data for external Python test

--- play/calibrate-play.tcl
+++ play/calibrate-play.tcl
@@ -22,6 +22,49 @@ apply {{} {
     set fd [open "folk-calibration-poses.txt" r]
     set calibrationPoses [read $fd]; close $fd
 
+    # Emit files into ~/aux/CameraCalibration/ for testing:
+    apply {{calibrationPoses} {
+        set cameraDir "$::env(HOME)/aux/CameraCalibration/folk-calibration-poses-camera-data"
+        catch { exec mkdir $cameraDir }
+
+        set model [dict get [lindex $calibrationPoses 0] model]
+        # Dictionary that maps (model point) -> list of up to NUM_POSES pose points.
+        set points [dict create]
+        dict for {id modelTag} $model {
+            foreach modelCorner [dict get $modelTag p] {
+                dict set points $modelCorner [list]
+            }
+        }
+
+        foreach calibrationPose $calibrationPoses {
+            # Emit camera data.
+            dict for {id cameraTag} [dict get $calibrationPose tags] {
+                set modelTag [dict get $calibrationPose model $id]
+                for {set i 0} {$i < 4} {incr i} {
+                    set modelPoint [lindex [dict get $modelTag p] $i]
+                    dict lappend points $modelPoint [lindex [dict get $cameraTag p] $i] 
+                }
+            }
+        }
+
+        set modelFd [open "$cameraDir/model.txt" w]
+        set poseFds [list]
+        for {set i 0} {$i < [llength $calibrationPoses]} {incr i} {
+            lappend poseFds [open "$cameraDir/$i.txt" w]
+        }
+        dict for {modelPoint posePoints} $points {
+            if {[llength $posePoints] < [llength $calibrationPoses]} {
+                continue
+            }
+            puts -nonewline $modelFd "$modelPoint "
+            foreach poseFd $poseFds posePoint $posePoints {
+                puts -nonewline $poseFd "$posePoint "
+            }
+        }
+
+        close $modelFd; foreach poseFd $poseFds { close $poseFd }
+    }} $calibrationPoses
+
     # Test without refinement (refiner is identity fn).
     calibrate $calibrationPoses {{poses calibration} {set calibration}}
 
