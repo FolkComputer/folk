5d4f060515ab24d064499d84301aacfc123b9b26
Omar Rizwan <omar@omar.website>
2024-01-23 17:54:07 +0800

    db: Make testQuery return Jim list

--- db.tcl
+++ db.tcl
@@ -1,4 +1,16 @@
 source "lib/c.tcl"
+proc assert condition {
+    set s "{$condition}"
+    if {![uplevel 1 expr $s]} {
+        set errmsg "assertion failed: $condition"
+        try {
+            if {[lindex $condition 1] eq "eq" && [string index [lindex $condition 0] 0] eq "$"} {
+                set errmsg "$errmsg\n[uplevel 1 [list set [string range [lindex $condition 0] 1 end]]] is not equal to [lindex $condition 2]"
+            }
+        } on error e {}
+        return -code error $errmsg
+    }
+}
 source "trie.tcl"
 set trieCc $cc
 
@@ -15,15 +27,19 @@ $cc proc testAssert {Db* db Jim_Obj* clauseObj} Statement* {
     free(c);
     return ret;
 }
-# TODO: Return list of Statement*.
-$cc proc testQuery {Db* db Jim_Obj* patternObj} Statement* {
+$cc proc testQuery {Db* db Jim_Obj* patternObj} Jim_Obj* {
     Clause* c = jimObjToClause(patternObj);
     ResultSet* rs = dbQuery(db, c);
     free(c);
-    assert(rs->nResults == 1);
-    Statement* ret = rs->results[0];
+
+    int nResults = (int) rs->nResults;
+    Jim_Obj* resultObjs[nResults];
+    for (size_t i = 0; i < rs->nResults; i++) {
+        Statement* result = rs->results[i];
+        resultObjs[i] = Jim_ObjPrintf("(Statement*) 0x%" PRIxPTR, (uintptr_t) result);
+    }
     free(rs);
-    return ret;
+    return Jim_NewListObj(interp, resultObjs, nResults);
 }
 $cc proc testGetClauseToStatementIdTrie {Db* db} Trie* {
     return dbGetClauseToStatementId(db);
@@ -71,11 +87,14 @@ if {[info exists ::argv0] && $::argv0 eq [info script]} {
         upvar db db
         set clause $args
         set stmt [testAssert $db $clause]
-        puts "$stmt ([testQuery $db $clause])"
+        assert {[list $stmt] eq [testQuery $db $clause]}
+        return $stmt
     }
     Test: This is a thing
     Test: This is another thing
     Test: Also x is true besides
+    assert {[llength [testQuery $db [list This is /some/ thing]]] == 2}
+
     trieWriteToPdf [testGetClauseToStatementIdTrie $db] trie.pdf; puts trie.pdf
     dbWriteToPdf [dbDotify $db] db.pdf; puts db.pdf
 }
