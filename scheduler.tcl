445093a2aa48164ace91f49dc7c34c4205cf88fb
Omar Rizwan <omar@omar.website>
2024-01-24 11:37:04 +0800

    scheduler: Start on setting up queue and shm. WIP: shm_init fails

--- scheduler.tcl
+++ scheduler.tcl
@@ -1,9 +1,62 @@
 source "lib/c.tcl"
 
 set cc [c create]
+
+$cc cflags -I./vendor/shm_malloc vendor/shm_malloc/libshm.a
+$cc include "shm_malloc.h"
+
+$cc cflags -I./vendor/libpqueue/src \
+    -Dmalloc=shm_malloc -Dfree=shm_free \
+    vendor/libpqueue/src/pqueue.c \
+    -Umalloc -Ufree
+$cc include "pqueue.h"
+
 $cc include <unistd.h>
 $cc code {
-    // TODO: Make C work queue.
+    typedef enum QueueOp { NONE, ASSERT, RETRACT } QueueOp;
+    typedef struct QueueEntry {
+        QueueOp op;
+        int seq;
+
+        union {
+            struct { Jim_Obj* clause } assert;
+            struct { Jim_Obj* pattern } retract;
+        };
+    } QueueEntry;
+
+    // TODO: Lock the workQueue.
+    // TODO: Allocate in shared memory.
+    pqueue_t* workQueue;
+    int seq;
+
+    int queueEntryCompare(pqueue_pri_t next, pqueue_pri_t curr) {
+        return next < curr;
+    }
+    pqueue_pri_t queueEntryGetPriority(void* a) {
+        QueueEntry* entry = a;
+        switch (entry->op) {
+            case NONE: return 0;
+            case ASSERT:
+            case RETRACT: return 80000 - entry->seq;
+        }
+        return 0;
+    }
+    void queueEntrySetPriority(void* a, pqueue_pri_t pri) {}
+    size_t queueEntryGetPosition(void* a) { return 0; }
+    void queueEntrySetPosition(void* a, size_t pos) {}
+}
+$cc proc init {} void {
+    if (shm_init(NULL, NULL) != 0) {
+        fprintf(stderr, "scheduler: init: shm_init failed\n");
+        abort();
+    }
+
+    workQueue = pqueue_init(16384,
+                            queueEntryCompare,
+                            queueEntryGetPriority,
+                            queueEntrySetPriority,
+                            queueEntryGetPosition,
+                            queueEntrySetPosition);
 }
 $cc proc process {} void {
     int pid = getpid();
@@ -27,6 +80,7 @@ $cc proc forque {} int {
 $cc compile
 
 # TODO: fork NUM_CPUS times
+init
 forque
 forque
 forque
