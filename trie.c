f30a307a6332dfe5d477bc6188ba4e05b55c34e8
Omar Rizwan <omar@omar.website>
2024-02-16 12:27:05 -0500

    Revert "Allow arbitrary options at end of When"
    This reverts commit 9420aed6bdc94561c8225112a2d535164c54bec0.

--- trie.c
+++ trie.c
@@ -192,8 +192,8 @@ static bool trieLookupImpl(bool doRemove, bool isLiteral,
             if (!isLiteral && trieScanVariable(trie->branches[j]->key, keyVarName, 100)) {
                 // Is the trie node a rest variable?
                 if (keyVarName[0] == '.' && keyVarName[1] == '.' && keyVarName[2] == '.') {
-                    trieLookupAll(trie->branches[j],
-                                  results, maxResults, resultsIdx);
+                    // FIXME: Fix rest variables in trie node
+                    /* lookupAll(results, resultsIdx, maxresults, trie->branches[j]); */
 
                 } else { // Or is the trie node a normal variable?
                     subtrieMatched = trieLookupImpl(doRemove, isLiteral,
