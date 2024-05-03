cfa2dd75e32b75b6ae409baded730d3b3b346924
Omar Rizwan <omar@omar.website>
2024-05-03 12:47:03 -0400

    c: Make C library internal state thread-local
    was unsafe multithreading before.

--- lib/c.tcl
+++ lib/c.tcl
@@ -74,8 +74,8 @@ class C {
         #include <stdio.h>
         #include <setjmp.h>
 
-        jmp_buf __onError;
-        static Jim_Interp* interp;
+        jmp_buf __thread __onError;
+        static __thread Jim_Interp* interp;
 
         #define __ENSURE(EXPR) if (!(EXPR)) { Jim_SetResultFormatted(interp, "failed to convert argument from Tcl to C in: " #EXPR); longjmp(__onError, 0); }
         #define __ENSURE_OK(EXPR) if ((EXPR) != JIM_OK) { longjmp(__onError, 0); }
