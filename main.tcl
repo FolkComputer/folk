f682f387079d09b1edba846164eb29f9d233c54a
Omar Rizwan <omar@omar.website>
2024-05-30 23:07:23 -0400

    Commit: Replace (globally) with (on X) syntax

--- main.tcl
+++ main.tcl
@@ -188,9 +188,10 @@ set ::toCommit [dict create]
 proc Commit {args} {
     upvar this this
     set body [lindex $args end]
-    if {[lindex $args 0] eq "(globally)"} {
-        set args [lreplace $args 0 0]
-        set key [list Commit {*}[lreplace $args end end]]
+    if {[lindex $args 0] eq "(on"} {
+        set userSpecifiedThis [string range [lindex $args 1] 0 end-1]
+        set args [lreplace $args 0 1]
+        set key [list Commit $userSpecifiedThis {*}[lreplace $args end end]]
     } else {
         set key [list Commit [expr {[info exists this] ? $this : "<unknown>"}] {*}[lreplace $args end end]]
     }
