1212ab4392f778aed8f0a914d651f5b5d85a9341
Omar Rizwan <omar@omar.website>
2025-05-05 23:19:37 -0400

    test/concurrent: Test shared object

--- test/concurrent.c
+++ test/concurrent.c
@@ -5,13 +5,27 @@
 #include <jim.h>
 #include <stdio.h>
 
+Jim_Obj* _Atomic sharedObj = NULL;
+
 void* thread_interpreter(void* arg) {
     Jim_Interp* interp = Jim_CreateInterp();
     Jim_RegisterCoreCommands(interp);
     Jim_InitStaticExtensions(interp);
-    
-    const char* script = "puts \"Hello, world!\"";
-    Jim_Eval(interp, script);
+
+    if (sharedObj == NULL) {
+        const char* script = "expr {10 * 2}";
+        Jim_Eval(interp, script);
+        sharedObj = Jim_GetResult(interp);
+
+    } else {
+        const char* lambda = "{sharedObj} {incr sharedObj; puts \"The result was $sharedObj\"}";
+        Jim_Obj* objv[] = {
+            Jim_NewStringObj(interp, "apply", -1),
+            Jim_NewStringObj(interp, lambda, -1),
+            sharedObj
+        };
+        Jim_EvalObjVector(interp, sizeof(objv)/sizeof(objv[0]), objv);
+    }
     
     Jim_FreeInterp(interp);
     return NULL;
