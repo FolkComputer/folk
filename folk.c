e7c38fd811ac83d1d4084fad925ea2e69f9306fc
Omar Rizwan <omar@omar.website>
2025-10-20 22:50:04 -0400

    Fix Tracy

--- folk.c
+++ folk.c
@@ -759,12 +759,11 @@ static void runBlock(Clause* bodyPattern, Clause* toUnifyWith, const char* body,
     int error;
     {
 #ifdef TRACY_ENABLE
-        const char *source = statementSourceFileName(when);
         char name[1000];
         int namesz = snprintf(name, 1000, "%s:%d",
-                              source, statementSourceLineNumber(when));
-        uint64_t srcloc = ___tracy_alloc_srcloc(statementSourceLineNumber(when),
-                                               source, strlen(source),
+                              sourceFileName, sourceLineNumber);
+        uint64_t srcloc = ___tracy_alloc_srcloc(sourceLineNumber,
+                                               sourceFileName, strlen(sourceFileName),
                                                name, namesz,
                                                0);
         TracyCZoneCtx ctx = ___tracy_emit_zone_begin_alloc(srcloc, 1);
